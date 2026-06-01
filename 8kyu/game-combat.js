e9a1760132684b6e270472bdc408f9917284894d
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-01 17:35:42 +0300

    game-combat.js

---
+++ 8kyu/game-combat.js
@@ -0,0 +1,11 @@
+//📌Grasshopper - Terminal game combat function
+// Create a combat function that takes the player's current health and ' +
+// 'the amount of damage received, and returns the player's new health.
+// Health can't be less than 0.
+//
+//✅
+// function combat(health, damage) {
+//   const newHealth = health - damage;
+//   return newHealth < 0 ? (`0, ${"Health cannot go below 0"}`) : newHealth;
+// }
+// console.log(combat(20, 30))
