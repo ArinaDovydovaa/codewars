26cc62d9e35c5d87e4435dd93d9d9f57a28c008f
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-01 17:34:24 +0300

    convert-boolean.js

---
+++ 8kyu/convert-boolean.js
@@ -0,0 +1,21 @@
+//📌Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string
+// for true, or a "No" string for false.
+//
+//✅
+// тернарный оператор
+// function boolToWord (bool){
+//   return bool ? "Yes" : "No";}
+//
+// console.log(boolToWord(true));   // "Yes"
+// console.log(boolToWord(false));  // "No"
+//
+//✅
+// if/else
+// function boolToWord( bool ){
+//   if( bool===true ){
+//     return "Yes";
+//   } else  {return "No";
+//   }
+// }
+// console.log(boolToWord(true))
