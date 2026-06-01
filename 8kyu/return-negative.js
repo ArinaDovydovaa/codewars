0066a01ed2b521ce8abd8bc92b3b81ac57d7df6e
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-01 17:32:38 +0300

    return-negative.js

---
+++ 8kyu/return-negative.js
@@ -0,0 +1,13 @@
+//📌Return Negative
+// In this simple assignment you are given a number and have to make it negative.
+// But maybe the number is already negative?
+//
+// ✅
+// function makeNegative(num) {
+//   return -Math.abs(num);
+// }
+//
+// console.log(makeNegative(1));    // return -1
+// console.log(makeNegative(-5));   // return -5
+// console.log(makeNegative(0));    // return 0
+// console.log(makeNegative(0.12)); // return -0.12
