f6faafecd7c38e4ed38e9fca4585cb3b1d930f9b
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-10 14:08:59 +0300

    mean-of-an-array

---
+++ 8kyu/mean-of-an-array.js
@@ -0,0 +1,13 @@
+//📌Найти среднее арифметическое элементов массива
+// и округлить его вниз (до ближайшего меньшего целого).
+//
+//✅
+function getAverage(marks){
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+  return Math.floor(sum / marks.length);
+}
+console.log(getAverage([2,2,2,2]));
+console.log(getAverage([1,2,3,4,5,]));
