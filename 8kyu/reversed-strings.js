847fd67ef81eea2c69d3f296ae855cfaf3a3ab9f
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-10 14:32:05 +0300

    reversed-strings

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,13 @@
+//📌Complete the solution so that it reverses the string passed into it.
+//Напишите функцию, которая принимает строку и возвращает эту же строку
+// в обратном порядке (перевернутую).
+//✅
+function solution(str){
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log(solution('world'));
+console.log(solution('hello'));
