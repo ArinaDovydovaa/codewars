bcedfcd4b7d4085c3b64e70ce6db467ae99b68d1
Arina Dovydovaa <arina.dovydova95@gmail.com>
2026-06-22 17:15:18 +0300

    add-property-to-every-object-in-array

---
+++ 7kyu/add-property-to-every-object-in-array.js
@@ -0,0 +1,19 @@
+//to add a new property usersAnswer to every object in the array questions.
+// The value of usersAnswer should be set to null.
+// The solution should work for array of any length.
+//
+//добавить новое свойство usersAnswer(равное null)
+// к каждому объекту в массиве questions
+var questions  = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+questions.forEach(question => question.usersAnswer = null);
+
+console.log(questions)
